e427b3dcc9155bd6bd900f4274bff34aa0423356
Unknown <andreas@stickydirt.se>
2019-01-08 11:25:24 +0100

    Add Basic registration & login. No cookies yet.

--- express/app.js
+++ express/app.js
@@ -2,67 +2,101 @@
   The express app is a global called app
   It can respond on all routes under /api
 */
-let app = global.expressApp;
+const app = global.expressApp;
+const bodyParser = require("body-parser");
+const User = require("./models/User");
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+mongoose
+  .connect(
+    "mongodb://localhost:27017/startmenow",
+    { useNewUrlParser: true }
+  )
+  .then(() => {
+    console.log("DB Connected!");
+  });
+
+app.use(bodyParser.json());
 
 // Set up socket.io
-const io = require('socket.io')(
-  global.httpServer, 
-  {
-    path: global.production ? '/api/socket' : '/socket',
-    serveClient: false
-  }
-);
+const io = require("socket.io")(global.httpServer, {
+  path: global.production ? "/api/socket" : "/socket",
+  serveClient: false
+});
 
 // Basic test of socket.io connectivity
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
+io.on("connection", function(socket) {
+  socket.on("chat message", function(msg) {
+    console.log("message: " + msg);
   });
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
+  socket.on("disconnect", function() {
+    console.log("user disconnected");
   });
 });
 
+app.post("/register", async (req, res) => {
+  console.log(req.body);
+  const email = req.body.email;
+  let password = req.body.password;
 
-// Some example data for the Todo app
-app.get('/todo-list-example-data',(req, res) => {
-  res.json([
-    {
-      "id": 0.14890674152580785,
-      "todo": "Create a project with React Warp Core",
-      "done": true
-    },
-    {
-      "id": 0.7582486745257166,
-      "todo": "Run npm start",
-      "done": true
-    },
-    {
-      "id": 0.4867703766375038,
-      "todo": "Continue testing",
-      "done": false
-    },
-    {
-      "id": 0.6641145526555352,
-      "todo": "Understand the code"
-    },
-    {
-      "id": 0.3006365270288174,
-      "todo": "Build something awesome"
-    },
-    {
-      "id": 0.6973592500276979,
-      "todo": "Start testing the Todo app",
-      "done": true
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+      password = hash;
+
+      // Check if user exists already
+      User.findOne({ email })
+        .then(user => {
+          if (user) {
+            return res
+              .status(400)
+              .json({ success: false, message: "User already exists!" });
+          }
+
+          console.log(password);
+
+          new User({
+            email,
+            password
+          })
+            .save()
+            .then(() => {
+              res.json("User registred");
+            });
+        })
+        .catch(err => console.error(err));
+    });
+  });
+
+  console.log(password);
+
+});
+
+app.get("/login", (req, res) => {
+
+  const email = req.body.email;
+  let password = req.body.password;
+
+  User.findOne({ email }).then(user => {
+    if (user) {
+      console.log(user.password);
+
+      bcrypt.compare(password, user.password, (err, pwMatch) => {
+        console.log(pwMatch);
+        
+        if(pwMatch) {
+          // Do login here
+        }
+  
+        res.json(pwMatch)
+      });
+    } else {
+      res.json('didnt find user..')
     }
-  ]);
-});
+   
+  })
 
-// Answer hello world on all routes
-app.all('*', (req, res) => {
-  res.json({
-    message: 'Hello world!',
-    method: req.method,
-    url: req.url
-  });
-});
+
+
+
+})
