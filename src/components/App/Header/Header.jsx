391f1d32cc6ccd05dc27fc5a0f524a8f37fe35aa
Unknown <andreas@stickydirt.se>
2019-01-10 17:49:59 +0100

    Add logoidea

--- src/components/App/Header/Header.jsx
+++ src/components/App/Header/Header.jsx
@@ -1,26 +1,40 @@
-<Container className="header d-flex justify-content-end py-4 px-2">
+<Container className='header d-flex justify-content-between py-3 px-3'>
+  <div className="logoholder ml-1">
+    <img src={require("./imgs/logo3.png")} alt='' />
+  </div>
 
-  <Link to='/' className="d-flex flex-column align-items-center mr-3 iconholder">  
-    <i className="fas fa-table"></i>
-    <p>Widgets</p>
+  <div className="d-flex justify-content-end p-0">
+    <Link
+      to='/'
+      className='d-flex flex-column justify-content-center align-items-center mr-3 iconholder'
+    >
+      <i className='fas fa-table' />
+      <p>Widgets</p>
+    </Link>
 
-  </Link>
-
-  <Link to='/profile' className="d-flex flex-column align-items-center mr-3 iconholder">  
-    <i className="fas fa-user"></i>
-    <p>Profile</p>
-
-  </Link>
-  {this.props.loginStore.isLoggedIn ? 
-    <div onClick={this.props.loginStore.logout} className="d-flex flex-column align-items-center mr-3 iconholder">  
-      <i className="fas fa-sign-out-alt"></i>
-      <p>Log out</p>
-    </div>
-    :
-    <div onClick={this.login} className="d-flex flex-column align-items-center mr-3 iconholder">  
-      <i className="fas fa-sign-in-alt"></i>
-      <p>Log in</p>
-    </div>
-  }
-
+    <Link
+      to='/profile'
+      className='d-flex flex-column justify-content-center align-items-center mr-3 iconholder'
+    >
+      <i className='fas fa-user' />
+      <p>Profile</p>
+    </Link>
+    {this.props.loginStore.isLoggedIn ? (
+      <div
+        onClick={this.props.loginStore.logout}
+        className='d-flex flex-column justify-content-center align-items-center mr-1 iconholder'
+        >
+        <i className='fas fa-sign-out-alt' />
+        <p>Log out</p>
+      </div>
+    ) : (
+      <div
+        onClick={this.login}
+        className='d-flex flex-column justify-content-center align-items-center mr-1 iconholder'
+        >
+        <i className='fas fa-sign-in-alt' />
+        <p>Log in</p>
+      </div>
+    )}
+  </div>
 </Container>
