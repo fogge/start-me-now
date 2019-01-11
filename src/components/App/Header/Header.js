d25b1fd2e8d53320b37a7adbc9ab3aa45a9e648e
Unknown <andreas@stickydirt.se>
2019-01-11 15:06:31 +0100

    Add clock & new logo which still sucks.

--- src/components/App/Header/Header.js
+++ src/components/App/Header/Header.js
@@ -1,7 +1,16 @@
 import './Header.scss';
 
-@inject('loginStore') export default class Header extends Component {
+@inject('loginStore') @observer export default class Header extends Component {
   async start() {}
+
+  @observable date = null;
+
+  componentDidMount() {
+    setInterval(
+      () => this.date = new Date(),
+      1000
+    );
+  }
 
 
 }
--- src/components/App/Header/Header.jsx
+++ src/components/App/Header/Header.jsx
@@ -1,38 +1,40 @@
-<Container className='header d-flex justify-content-between py-3 px-3'>
+<Container className="header d-flex justify-content-between align-items-center py-3 px-3">
   <div className="logoholder ml-1">
-    <img src={require("./imgs/logo3.png")} alt='' />
+    <img src={require('./imgs/logo.png')} alt="" />
   </div>
+
+  <Clock value={this.date} className="clock" size="100" />
 
   <div className="d-flex justify-content-end p-0">
     <Link
-      to='/'
-      className='d-flex flex-column justify-content-center align-items-center mr-3 iconholder'
+      to="/"
+      className="d-flex flex-column justify-content-center align-items-center mr-3 iconholder"
     >
-      <i className='fas fa-table' />
+      <i className="fas fa-table" />
       <p>Widgets</p>
     </Link>
 
     <Link
-      to='/profile'
-      className='d-flex flex-column justify-content-center align-items-center mr-3 iconholder'
+      to="/profile"
+      className="d-flex flex-column justify-content-center align-items-center mr-3 iconholder"
     >
-      <i className='fas fa-user' />
+      <i className="fas fa-user" />
       <p>Profile</p>
     </Link>
     {this.props.loginStore.isLoggedIn ? (
       <div
         onClick={this.props.loginStore.logout}
-        className='d-flex flex-column justify-content-center align-items-center mr-1 iconholder'
-        >
-        <i className='fas fa-sign-out-alt' />
+        className="d-flex flex-column justify-content-center align-items-center mr-1 iconholder"
+      >
+        <i className="fas fa-sign-out-alt" />
         <p>Log out</p>
       </div>
     ) : (
       <div
         onClick={this.login}
-        className='d-flex flex-column justify-content-center align-items-center mr-1 iconholder'
-        >
-        <i className='fas fa-sign-in-alt' />
+        className="d-flex flex-column justify-content-center align-items-center mr-1 iconholder"
+      >
+        <i className="fas fa-sign-in-alt" />
         <p>Log in</p>
       </div>
     )}
